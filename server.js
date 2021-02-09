d8485545eb016e786cb569f81a90da8070ad9608
Midhat Lavic <mla@celum.com>
2021-02-09 13:08:18 +0100

    deploying to heroku revert

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-app.use(express.static(__dirname + '/dist/reeinvent-fe'));
-app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+
-                         '/dist/reeinvent-fe/index.html'));});
-app.listen(process.env.PORT || 8080);
--- src/app/services/synonym-dictionary-service.ts
+++ src/app/services/synonym-dictionary-service.ts
@@ -3,22 +3,19 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { WordSynonymsPair } from '../model/word-synonym-pair';
-import {environment} from '../../environments/environment';
 
 @Injectable({providedIn: 'root'})
 export class SynonymDictionaryService {
 
-  private baseUrl = environment.baseUrl;
-
   constructor(private httpClient: HttpClient) {}
 
   public getSynonyms(word: string): Observable<string[]> {
-    const getSynonymsEndpoint = this.baseUrl + `synonym/${word}`;
+    const getSynonymsEndpoint = `synonym/${word}`;
 
     return this.httpClient.get<string[]>(getSynonymsEndpoint);
   }
 
   public addSynonyms(wordSynonymsPair: WordSynonymsPair[]): Observable<boolean> {
-    return this.httpClient.post<boolean>(this.baseUrl + 'synonym', wordSynonymsPair);
+    return this.httpClient.post<boolean>('synonym', wordSynonymsPair);
   }
 }
